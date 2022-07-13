bf04575c452bd63fa59b4bf9f9d7ef70b45a026b
avinash <pandeyavinash960@gmail.com>
2022-07-13 18:30:13 +0530

    add it

--- language/forcpp.cpp
+++ language/forcpp.cpp
@@ -1,14 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-<<<<<<< HEAD
-
-=======
->>>>>>> other
 int x;
 cin>>x;
 int sum =0; 
-for(int i =1; i<=x; i++)
+for(int i =1; i<x; i++)
 sum = sum +  i;
 cout<<sum;
 return 0;
