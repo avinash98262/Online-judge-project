4436b1f4b9cd2452d275af49b28e3df03f1f578d
avinash <pandeyavinash960@gmail.com>
2022-07-14 16:28:01 +0530

    adding css and docker

--- language/forcpp.cpp
+++ language/forcpp.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
-using namespace std;
+using namespace std; 
 int main(){
-int x;
-cin>>x;
+int x; 
+cin>>x; 
 int sum =0; 
-for(int i =1; i<=x; i++)
-sum = sum +  i;
+for(int i= 1; i<x; i++){
+sum += i; 
+}
 cout<<sum;
-return 0;
+return 0; 
 }
