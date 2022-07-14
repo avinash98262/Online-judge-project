c4f45f14332ea36af6a680fa1328b01c3b728bf7
avinash <pandeyavinash960@gmail.com>
2022-07-14 19:53:54 +0530

    change

--- language/forcpp.cpp
+++ language/forcpp.cpp
@@ -4,14 +4,9 @@ int main(){
 int x; 
 cin>>x; 
 int sum =0; 
-<<<<<<< HEAD
-for(int i =1; i<x; i++)
-sum = sum +  i;
-=======
-for(int i= 1; i<x; i++){
+for(int i= 1; i<=x; i++){
 sum += i; 
 }
->>>>>>> other
 cout<<sum;
 return 0; 
 }
