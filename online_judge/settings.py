acec694543296d7fb02e094b3f5c9b044581024b
avinash <pandeyavinash960@gmail.com>
2022-07-18 00:28:57 +0530

    debug

--- online_judge/settings.py
+++ online_judge/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g67jyk@)^)%auy346jmq3fc#$9-!fme5zkk0g106h2si&#-k1*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
