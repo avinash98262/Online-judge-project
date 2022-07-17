ef32d6bc9d493842288310be4b8849b0403cb82d
avinash <pandeyavinash960@gmail.com>
2022-07-17 10:08:16 +0530

    one more time

--- online_judge/settings.py
+++ online_judge/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os 
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +29,7 @@ SECRET_KEY = 'django-insecure-g67jyk@)^)%auy346jmq3fc#$9-!fme5zkk0g106h2si&#-k1*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','online-judge-live.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,8 +127,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join('BASE_DIR', 'media')
-MEDIA_URL ='/media/'
+STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
+# MEDIA_ROOT = os.path.join('BASE_DIR', 'media')
+# MEDIA_URL ='/media/'
+django_heroku.settings(locals())
 
 
 STATICFILES_DIRS = [
